56812584521e0924e58465a13d47f349d2f1c3b2
sriv1976 <svelaga@outlook.com>
2025-09-16 20:40:53 -0400

    Final Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,20 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    
+    '''
+    This function takes input from index.html
+    '''
+    if text_to_analyse.strip() == "":
+        formatted_response = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+        return formatted_response
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -11,7 +24,7 @@ def emotion_detector(text_to_analyse):
     
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-
+    
     # Check for response status code of 200, then extract scores and dominant emotion
     if response.status_code == 200:
         
@@ -35,8 +48,15 @@ def emotion_detector(text_to_analyse):
             "sadness": sadness_score,
             "dominant_emotion": dominant_emotion
         }
-    elif response.status_code == 500:
-        formatted_response == None
+    elif response.status_code == 400:
+        formatted_response = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
 
     return(formatted_response)
     
--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+'''
+This application will be acccessed from localhost:5000 and provides user ability to enter text
+and responds with scores for different emotions and also the dominant emotion for the text
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,20 +10,33 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 
-def emotionDetector():
+def emotion_detect():
+    '''
+    This function takes the input of the user and performs emotion detection analysis
+    '''
     # Retrieve the input text from request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Obtain emotion scores and dominant by passing input text to Emotion Detector 
+    # Obtain emotion scores and dominant by passing input text to Emotion Detector
     response = emotion_detector(text_to_analyze)
-    if response is None:
+
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        return "For the given statement, the system response is 'anger': {}, 'disgust' : {},'fear': {},'joy' : {} and 'sadness': {}. The dominant emotion is {}".format(response['anger'], response['disgust'], response['fear'],response['joy'],response['sadness'],response['dominant_emotion'])
-    
+    return f"""
+    For the given statement, the system response is 
+    'anger': {response['anger']},
+    'disgust' : {response['disgust']},
+    'fear': {response['fear']},
+    'joy' : {response['joy']} and 
+    'sadness': {response['sadness']}
+    . The dominant emotion is {response['dominant_emotion']}
+    """
 
 @app.route("/")
 def render_index_page():
+    '''
+    This renders the default page for user entry 
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
